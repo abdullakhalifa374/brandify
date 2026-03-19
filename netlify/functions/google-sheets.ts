7027e8e85a6e8e14ed993929608f0255d975613d
abdullakhalifa374 <166983107+abdullakhalifa374@users.noreply.github.com>
2026-03-19 03:47:08 +0300

    Update google-sheets.ts

--- netlify/functions/google-sheets.ts
+++ netlify/functions/google-sheets.ts
@@ -25,10 +25,9 @@ export const handler = async (event: any) => {
         client_email: process.env.GOOGLE_CLIENT_EMAIL,
         private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
       },
-      scopes: [
+scopes: [
         'https://www.googleapis.com/auth/spreadsheets',
-        'https://www.googleapis.com/auth/drive.readonly',
-        'https://www.googleapis.com/auth/drive.file' // NEW: Added permission to upload files
+        'https://www.googleapis.com/auth/drive' 
       ],
     });
 
