496dd3cf400a8511bb2227aa28bd1d524862dbda
abdullakhalifa374 <166983107+abdullakhalifa374@users.noreply.github.com>
2026-04-08 15:42:38 +0300

    Update PublicNavbar.tsx

--- src/components/PublicNavbar.tsx
+++ src/components/PublicNavbar.tsx
@@ -12,9 +12,8 @@ const PublicNavbar = () => {
   ];
 
   return (
-    <header className="sticky top-0 z-50 border-b border-border bg-card/80 backdrop-blur-sm w-full">
+    <header className="sticky top-0 z-50 bg-white border-b border-[#E2E7F5] w-full">
       <div className="container flex h-14 items-center justify-between">
-        {/* NEW: Image Logo */}
         <Link to="/" className="flex items-center">
           <img 
             src="https://cloud-1de12d.becdn.net/media/iW=238&iH=54&oX=0&oY=0&cW=238&cH=54/3f67b33762851b549b689cda2577b6ca/Brandify-Logo.png" 
